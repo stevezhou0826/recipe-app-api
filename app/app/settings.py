9b447061ff06895d9bc7e172dcf3440a28277032
Steve Zhou <steve.zhou@ey.com>
2024-08-16 12:30:25 -0400

    11.64

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
+    
     'rest_framework',
     'drf_spectacular',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -133,5 +134,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'core.User'
 
 REST_FRAMEWORK = {
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.Autoschema',
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
