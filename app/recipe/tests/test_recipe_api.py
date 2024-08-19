53a6f6f7ed4f865d8f381a0d70dacb0ceceee92b
Steve Zhou <steve.zhou@ey.com>
2024-08-19 13:53:05 -0400

    13.86 right

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -9,7 +9,7 @@ from django.test import TestCase
 from django.urls import reverse
 
 from rest_framework import status
-from rest_framework import APIClient
+from rest_framework.test import APIClient
 
 from core.models import Recipe
 
