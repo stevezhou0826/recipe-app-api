16e1f6ebee8c871504ec3f693255aac60f1d0aeb
Steve Zhou <steve.zhou@ey.com>
2024-08-19 14:29:19 -0400

    13.84 wrong

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+"""
+Views for the recipe apis
+"""
 
-# Create your views here.
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from core.models import Recipe
+from recipe import serializers
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """View for manage recipe APIs"""
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Recipe.objects.all()
+    serializer_class = serializers.RecipeDetailSerializer
+
+    def get_queryset(self):
+        """Return objects for the current authenticated user only"""
+        return self.queryset.filter(user=self.request.user).order_by('-id')
+    
+    def get_serializer_class(self):
+        """Return the serializer class for request"""
+        if self.action == 'list':
+            return serializers.RecipeSerializer
+        return self.serializer_class
