01e2c0476d1c3e401da6a4f602daa2c4822b7bcd
Steve Zhou <steve.zhou@ey.com>
2024-08-22 14:29:45 -0400

    wrong wrong

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -5,8 +5,7 @@ URLs for recipe app
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from recipe import views
-from django.conf.urls.static import static
-from django.conf import settings
+
 
 router = DefaultRouter()
 router.register('recipes', views.RecipeViewSet)
@@ -18,8 +17,3 @@ urlpatterns = [
     path('', include(router.urls)),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, 
-        document_root=settings.MEDIA_ROOT
-    )
